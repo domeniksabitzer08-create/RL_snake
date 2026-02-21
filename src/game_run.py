24061d0070eb03258151ebbeed5b8e96152201fc
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-02-21 21:29:02 +0100

    Snake game logic done added game over with reset

--- src/game_run.py
+++ src/game_run.py
@@ -14,6 +14,17 @@ def show_grid():
             cell = pygame.Rect((i * Grid.cell_size)+Grid.start_pos.x, (j* Grid.cell_size)+Grid.start_pos.y, Grid.cell_render_width, Grid.cell_render_width)
             pygame.draw.rect(screen, (200,0,0), cell)
 
+### GAME FUNCTIONS ###
+def reset():
+    score = 0
+    snake = snake_classes.SnakeManager(Vector2D(3, 4), Vector2D.right, 3)
+
+### Timing Variables ###
+clock = pygame.time.Clock()
+current_time = pygame.time.get_ticks()
+last_update = 0
+update_interval = 250
+
 # Vector directions
 Vector2D.left = Vector2D(-1, 0)
 Vector2D.right = Vector2D(1, 0)
@@ -27,11 +38,18 @@ snake = snake_classes.SnakeManager(Vector2D(3,4),Vector2D.right, 3)
 while True:
     screen.fill((0, 0, 0))
     show_grid()
-    snake.move_step()
-    #handel_input()
+    # Call move_step depending on the update_interval
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= update_interval:
+        snake.move_step()
+        last_update = current_time
+    snake.handle_input()
+    # Manage game over
+    if snake.is_game_over:
+        reset()
+        snake = snake_classes.SnakeManager(Vector2D(3, 4), Vector2D.right, 3)
 
-
-    snake.render_parts(screen)
+    snake.render_objects(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- src/snake_classes.py
+++ src/snake_classes.py
@@ -1,7 +1,12 @@
+from types import NoneType
+
 import pygame
 from dataclasses import dataclass
 import time
 from random import randint
+
+from PIL.ImageChops import screen
+
 
 ### VECTOR CLASS ###
 
@@ -57,7 +62,7 @@ def grid_to_screen_pos(pos: Vector2D):
 ### Snake
 part_color = (0,255,0)
 # How much the color decreases over time
-part_color_reduction_rate = 30
+part_color_reduction_rate = 5
 # Size of the parts
 snake_part_render_size = 25
 
@@ -95,21 +100,29 @@ class Food:
     def __init__(self, pos: Vector2D):
         self.pos = pos
     def render(self, screen: pygame.Surface):
-        food = pygame.Rect(0, 0, food_render_size, food_render_size)
+        screen_pos = grid_to_screen_pos(self.pos)
+        food = pygame.Rect(screen_pos.x, screen_pos.y, food_render_size, food_render_size)
         pygame.draw.rect(screen, food_color, food)
 
 class SnakeManager:
     """Manages spawning and moving of the parts"""
     def __init__(self, start_pos: Vector2D, start_direction: Vector2D, n_starting_parts: int):
+        # Snake
         self.start_pos = start_pos
         self.start_direction = start_direction
         self.n_starting_parts = n_starting_parts
         self.part_list = []
         self.direction = start_direction
         self.is_dir_changing = False
+        # Food
+        self.food = None
         self.init_parts()
+        # Other
+        self.is_game_over = False
+
 
     def init_parts(self):
+        """Init all parts and the first food"""
         # Init first part
         self.part_list.append(SnakePart(self.part_list))
         self.part_list[0].pos = self.start_pos
@@ -118,23 +131,73 @@ class SnakeManager:
             self.part_list.append(SnakePart(self.part_list))
             self.part_list[i + 1].pos = Vector2D(self.start_pos.x +  ((i+1) * self.start_direction.x*-1),
                                                  self.start_pos.y + ((i+1) * self.start_direction.y*-1))
+        # Init food
+        self.food = self.init_food()
 
     def move_step(self):
-        time.sleep(0.3)
-
         # Move each part to the position of the part before
         for i in range(len(self.part_list),1,-1):
-            print(f"i:{i} | len: {len(self.part_list)}")
             self.part_list[i-1].move_to_pre_part()
         # Move the first part
         self.part_list[0].move_to_direction(self.direction)
         self.is_dir_changing = False
 
+        ### Check Collisions ##
+        # Check if head collides with food
+        self.check_food_collision()
+        # Check if Snake collides with other part of snake
+        if self.check_other_part_collision(self.part_list[0]):
+            self.game_over()
+        # Check if snake head collides with border
+        self.check_border_collision()
+
+
+    def check_food_collision(self):
+        """Adds a Part if head collides with food and init a new food"""
+        if self.part_list[0].pos == self.food.pos:
+            self.add_part()
+            self.food = self.init_food()
+
+    def init_food(self) -> Food:
+        """Init on a random pos and returns the food object"""
+        # Create one instance of Food on rnd pos
+        spawn_pos = Vector2D(randint(0,Grid.cell_count-1), randint(0,Grid.cell_count-1))
+        food = Food(spawn_pos)
+        # If it collides with a part it will try again
+        while self.check_other_part_collision(food):
+            spawn_pos = Vector2D(randint(0, Grid.cell_count-1), randint(0, Grid.cell_count-1))
+            food = Food(spawn_pos)
+        return food
+
+    def add_part(self):
+        """Init a new part in the next tick"""
+        self.part_list.append(SnakePart(self.part_list))
+
+    def check_other_part_collision(self, object: SnakePart | Food):
+        """Returns True if the object collides with any other part of the snake."""
+        for part in self.part_list:
+            if part.pos == object.pos and part != object:
+                return True
+        return False
+
+    def check_border_collision(self):
+        x = self.part_list[0].pos.x
+        y = self.part_list[0].pos.y
+        if  x > Grid.cell_count-1 or x < 0 :
+            self.game_over()
+        if y > Grid.cell_count-1 or y < 0 :
+            self.game_over()
 
     def change_direction(self, new_direction: Vector2D):
         if not (new_direction*-1) == self.direction and not self.is_dir_changing:
             self.is_dir_changing = True
             self.direction = new_direction
+
+
+
+    def game_over(self):
+        print("Game Over")
+        self.is_game_over = True
 
     def handle_input(self):
         if pygame.key.get_pressed()[pygame.K_w]:
@@ -147,10 +210,14 @@ class SnakeManager:
             self.change_direction(Vector2D.left)
 
 
-    def render_parts(self, screen: pygame.Surface):
+    def render_objects(self, screen: pygame.Surface):
         # Render all parts
         for part in self.part_list:
-            part.render(screen)
+            if part.pos:
+                part.render(screen)
+        # Render food
+            self.food.render(screen)
+
 
 
 
