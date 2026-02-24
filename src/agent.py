b1fd905b5b31879a8a86ac28e0730417297dbde4
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-02-24 14:57:31 +0100

    fixed shape issus, send state to model

--- src/agent.py
+++ src/agent.py
@@ -1,8 +1,12 @@
 from random import randint
 from time import sleep
+import torch
+import torchvision
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+from torch import transpose
 
-from sympy.codegen.cnodes import static
-
+from cnn import  CNN
 import snake_classes
 from snake_classes import *
 
@@ -12,20 +16,35 @@ Vector2D.right = Vector2D(1, 0)
 Vector2D.up = Vector2D(0, -1)
 Vector2D.down = Vector2D(0, 1)
 
-env = SnakeManager(Vector2D(3,4),Vector2D.right,4, render=False)
-game_over = False
-action = [0,1,0]
+# Game Enviroment
+env = SnakeManager(Vector2D(3,4),Vector2D.right,4, render=True)
+
 score = 100
 
-# DEBUG FUNCTIONS
-def model(state):
-    return [randint(0,1),randint(0,1),randint(0,1)]
-
-while not game_over:
-    sleep(0.01)
-    state, reward, game_over, score = env.step(action)
-    action = model(state)
+# Model
+model = CNN(1,3,10)
 
 
+def train():
+    game_over = False
+    action = [0, 1, 0]
+    while not game_over:
+        sleep(0.01)
+        state, reward, game_over, score = env.step(action)
+        # Convert state to Tensor
+        state = torchvision.transforms.ToTensor()(state)
+        # Divide numbers so they are in range 0-1
+        state = state / 3
+        # Convert the datatype
+        state = state.type(torch.float32)
+        # Add an extra dim (conv2d expects: (batch, channels, height, width))
+        state = state.unsqueeze(0)
+        # Pass data through the model
+        y_logit = model(state)
+        # from logit -> to prediction -> to action
+        y_pred = torch.argmax(y_logit)
+        action = [0,0,0]
+        action[y_pred.item()] = 1
 
-
+if __name__ == '__main__':
+    train()
--- src/cnn.py
+++ src/cnn.py
@@ -15,17 +15,19 @@ class CNN(nn.Module):
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features=hidden_units*4, out_features=output_shape),
+            nn.Linear(in_features=hidden_units*5*5, out_features=output_shape),
         )
 
     def forward(self, x) ->  torch.Tensor:
         x = self.conv_block_1(x)
-        print(f"shape after block 1: {x.shape}")
+        #print(f"shape after block 1: {x.shape}")
         x = self.classifier(x)
         return x
 
 if __name__ == "__main__":
-    model = CNN(input_shape=3, output_shape=3)
-    dummy_tensor = torch.rand(1,3,5,5)
+    model = CNN(input_shape=1, output_shape=3)
+    dummy_tensor = torch.rand(1,1,5,5)
+    print(f"dummy input: {dummy_tensor}")
     y_logit = model(dummy_tensor)
     print(f"logit: {y_logit}")
+    print(f"shape logit: {y_logit.shape}")
